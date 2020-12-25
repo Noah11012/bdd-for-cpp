a93b6523ae9aaf482ecff12260f0d88e7900fa70
Noah <1101qbtext@gmail.com>
2020-12-24 23:19:10 -0500

    Removed a lot of underscores.

--- bdd-for-cpp.h
+++ bdd-for-cpp.h
@@ -16,18 +16,18 @@ struct __Bdd_Check__ {
 
 struct __Bdd_Single_Global_Context__ {
     // So we can make the output look pretty widepeepoHappy
-    char const *__color_red__   = "\033[31m";
-    char const *__color_blue__  = "\033[34m";
-    char const *__color_reset__ = "\033[0m";
-    char const *__current_color__;
+    char const *color_red   = "\033[31m";
+    char const *color_blue  = "\033[34m";
+    char const *color_reset = "\033[0m";
+    char const *current_color;
 
-    vector<__Bdd_Check__> __checks__;
-    vector<__Bdd_Check__> __failed_checks__;
+    vector<__Bdd_Check__> checks;
+    vector<__Bdd_Check__> failed_checks;
 
-    char const     *__current_what__;
-    vector<string>  __list_of_whats__;
+    char const     *current_what;
+    vector<string>  list_of_whats;
 
-    bool __run_once__ = true;
+    bool run_once = true;
 };
 
 static __Bdd_Single_Global_Context__ __bdd_gc__;
@@ -36,8 +36,8 @@ static __Bdd_Single_Global_Context__ __bdd_gc__;
 
 static void __printf__(char const *format, ...) { // @Refactor: Setting the current color then printing doesn't seem all that great
 #ifndef NO_BDD_COLOR_OUTPUT
-    if (__bdd_gc__.__current_color__) {
-        printf("%s", __bdd_gc__.__current_color__);
+    if (__bdd_gc__.current_color) {
+        printf("%s", __bdd_gc__.current_color);
     }
 #endif
     
@@ -49,8 +49,8 @@ static void __printf__(char const *format, ...) { // @Refactor: Setting the curr
     va_end(args);
     
 #ifndef NO_BDD_COLOR_OUTPUT
-    printf("%s", __bdd_gc__.__color_reset__);
-    __bdd_gc__.__current_color__ = nullptr;
+    printf("%s", __bdd_gc__.color_reset);
+    __bdd_gc__.current_color = nullptr;
 #endif
 }
 
@@ -61,7 +61,7 @@ extern char const *__spec_name__;
 
 int main() {
     if (__bdd_function_is_defined__ == false) {
-        __bdd_gc__.__current_color__ = __bdd_gc__.__color_red__;
+        __bdd_gc__.current_color = __bdd_gc__.color_red;
         __printf__("Error: ");
         __printf__("No specification block has been defined in this file!\n");
         return 1;
@@ -71,58 +71,58 @@ int main() {
     __bdd_function__("", &test_count); // First do a preliminary pass to see how many tests there are
     
     if (test_count == 0) {
-        __bdd_gc__.__current_color__ = __bdd_gc__.__color_blue__;
+        __bdd_gc__.current_color = __bdd_gc__.color_blue;
         __printf__("Warning: ");
         __printf__("No it() blocks created\n");
         return 1;
     }
     
-    __bdd_gc__.__current_color__ = __bdd_gc__.__color_blue__;
+    __bdd_gc__.current_color = __bdd_gc__.color_blue;
     __printf__("%s\n", __spec_name__);
     
     // Make sure everything is lined up properly
     size_t justify_amount = 0;
-    for (auto const &what : __bdd_gc__.__list_of_whats__) {
+    for (auto const &what : __bdd_gc__.list_of_whats) {
         if (what.size() > justify_amount) {
             justify_amount = what.size();
         }
     }
     
-    __bdd_gc__.__failed_checks__.clear();
+    __bdd_gc__.failed_checks.clear();
     
     int i = 0;
     while (i < test_count) { // Now run each test
-        __bdd_gc__.__checks__.clear();
-        __bdd_function__(__bdd_gc__.__list_of_whats__[i], nullptr);
+        __bdd_gc__.checks.clear();
+        __bdd_function__(__bdd_gc__.list_of_whats[i], nullptr);
         
-        if (__bdd_gc__.__checks__.size() == 0) {
-            __bdd_gc__.__current_color__ = __bdd_gc__.__color_blue__;
+        if (__bdd_gc__.checks.size() == 0) {
+            __bdd_gc__.current_color = __bdd_gc__.color_blue;
             __printf__("Warning: ");
-            __printf__("Zero checks within '%s'\n", __bdd_gc__.__current_what__);
+            __printf__("Zero checks within '%s'\n", __bdd_gc__.current_what);
             return 1;
         }
         
         size_t checks_passed = 0;
-        for (auto const &check : __bdd_gc__.__checks__) {
+        for (auto const &check : __bdd_gc__.checks) {
             if (check.condition) {
                 checks_passed++;
             } else {
-                __bdd_gc__.__failed_checks__.push_back(check);
+                __bdd_gc__.failed_checks.push_back(check);
             }
         }
         
         // Finally print the score
-        bool passed = checks_passed == __bdd_gc__.__checks__.size();
-        __printf__("\t%-*s ", justify_amount, __bdd_gc__.__current_what__);
-        __printf__("[%d/%d] %s\n", checks_passed, __bdd_gc__.__checks__.size(), passed ? "(PASSED)" : "(FAILED)");
+        bool passed = checks_passed == __bdd_gc__.checks.size();
+        __printf__("\t%-*s ", justify_amount, __bdd_gc__.current_what);
+        __printf__("[%d/%d] %s\n", checks_passed, __bdd_gc__.checks.size(), passed ? "(PASSED)" : "(FAILED)");
         
         i++;
     }
     
     // Print out any failed checks
-    if (__bdd_gc__.__failed_checks__.size() > 0) {
+    if (__bdd_gc__.failed_checks.size() > 0) {
         printf("\n");
-        for (auto const &check : __bdd_gc__.__failed_checks__) {
+        for (auto const &check : __bdd_gc__.failed_checks) {
             printf("\tFailed check() at line %d\n", check.line_number);
         }
     }
@@ -132,18 +132,18 @@ int main() {
 bool __bdd_function_is_defined__ = true;                                \
 void __bdd_function__(std::string const &__what__, int *__test_count__) \
 
-#define run_once() for (; __bdd_gc__.__run_once__; __bdd_gc__.__run_once__ = false)
+#define run_once() for (; __bdd_gc__.run_once; __bdd_gc__.run_once = false)
 
 #define it(what)                                                                           \
-if (__test_count__) { (*__test_count__)++; __bdd_gc__.__list_of_whats__.push_back(what); } \
+if (__test_count__) { (*__test_count__)++; __bdd_gc__.list_of_whats.push_back(what); }     \
 for (bool __run__ = (strcmp(what, __what__.c_str()) == 0);                                 \
 __run__ &&                                                                                 \
-(__run__ ? __bdd_gc__.__current_what__ = what : 0);                                                   \
+(__run__ ? __bdd_gc__.current_what = what : 0);                                            \
 __run__ = false)                                                                           \
 
 static bool __bdd_condition__;
 static bool __push_check__(bool condition, int line_number) {
-    __bdd_gc__.__checks__.push_back({ condition, line_number });
+    __bdd_gc__.checks.push_back({ condition, line_number });
     return __bdd_condition__;
 }
 
